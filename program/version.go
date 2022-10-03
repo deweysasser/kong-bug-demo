580fcb406c7da072630d5b42cfa876bc280624f5
Dewey Sasser <dsasser@actbluetech.com>
2022-10-03 12:05:45 -0400

    Update versions and names for fresh project

--- program/version.go
+++ program/version.go
@@ -2,7 +2,7 @@ package program
 
 import "fmt"
 
-// Version is created by the Makefile and passed in as a linker flag.  When go 1.18 is released, this will be replaced
+// Version is created by the Makefile and passed in as a linker flag.  When go 1.19 is released, this will be replaced
 // with the built-in mechanism
 
 var Version = "unknown"
